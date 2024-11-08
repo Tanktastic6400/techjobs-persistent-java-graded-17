1d5106f1e1a4d44c472416398999d1934066c885
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2024-11-07 20:29:25 -0600

    task 3 and tests are completed application is still not displaying fully, been told that it probably should display things once all tasks are done and working.

--- queries.sql
+++ queries.sql
@@ -3,5 +3,5 @@ SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA
 --Part 2
 SELECT name From employer WHERE location = "St. Louis City";
 --Part 3
-
+Drop Table job;
 --Part 4
