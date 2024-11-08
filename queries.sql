c009ec5864fbea50e99486f6628ad42cab0c532d
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2024-11-07 18:42:48 -0600

    task two is complete all tests in task 2 are passing. Having weird error of Jobs not rendering yet even though table is not empty

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 --Part 1
-
+SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'techjobs' AND TABLE_NAME = 'job';
 --Part 2
-
+SELECT name From employer WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    public Employer(){}
+
+    @NotBlank
+    @Size(max=50)
+    private String location;
+
+
+
+    public @NotBlank @Size(max = 50) String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotBlank @Size(max = 50) String location) {
+        this.location = location;
+    }
+
+
+
 }
